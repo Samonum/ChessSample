bedc00a7882966aca05c3c7d53aa1c0aa8a1eae6
Samonum <sam_mie_sam@hotmail.com>
2024-05-21 20:20:55 +0200

    Added the base project

---
+++ Chess/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Numerics;
+
+class Chess
+{
+    static void Main(string[] args)
+    {
+        string[,] boardState = {{"♜", "♞", "♝", "♛", "♚", "♝", "♞", "♜"},
+                                {"♟", "♟", "♟", "♟", "♟", "♟", "♟", "♟"},
+                                {"　", "　", "　", "　", "　", "　", "　", "　"},
+                                {"　", "　", "　", "　", "　", "　", "　", "　"},
+                                {"　", "　", "　", "　", "　", "　", "　", "　"},
+                                {"　", "　", "　", "　", "　", "　", "　", "　"},
+                                {"♙", "♙", "♙", "♙", "♙", "♙", "♙", "♙"},
+                                {"♖", "♘", "♗", "♕", "♔", "♗", "♘", "♖"}};
+
+        while(true)
+        {
+            PrintBoard(boardState);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What piece do you want to move?");
+            Console.Write("x y: ");
+            
+            string input = Console.ReadLine();
+            if(input[0] == 'Q' || input[0] == 'q')
+                break;
+            
+            string[] splitInput = input.Split();
+
+            int selectX = int.Parse(splitInput[0]); 
+            int selectY = int.Parse(splitInput[1]); 
+
+            Console.WriteLine("You slected "+ boardState[selectY, selectX] +" Where do you want to move?");
+            Console.Write("x y: ");
+             
+            input = Console.ReadLine();
+            if(input[0] == 'Q' || input[0] == 'q')
+                break;
+            
+            splitInput = input.Split();
+
+            int targetX = int.Parse(splitInput[0]);
+            int targetY = int.Parse(splitInput[1]);
+
+            boardState[targetY,targetX] = boardState[selectY,selectX];
+            boardState[selectY,selectX] = "　";
+
+        }
+    }
+
+
+    static void PrintBoard(string[,] board)
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            Console.Write(i + " ");
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Console.Write("|" + board[i, j]);
+            }
+            
+            Console.WriteLine("|");
+        }
+
+        Console.Write(" ");
+        for(int i = 0; i < board.GetLength(1); i++)
+            Console.Write("  " + (i).ToString());
+        Console.WriteLine(" ");
+    }
+}
